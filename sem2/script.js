313ba5b861dd35afd4afcebe7881b3537d557ae9
malinabooy <1anejlbcuh4uk1@gmail.com>
2024-03-19 11:59:15 +0300

    hw2

---
+++ sem2/script.js
@@ -0,0 +1,38 @@
+// let age = prompt('Сколько вам лет?');
+// console.log(`Вам ${age} лет`);
+
+// let userName = prompt('Как вас зовут?');
+// console.log(`Добро пожаловать! ${userName}`);
+
+// console.log(13 % 5); //3
+// let a;
+// alert(a); //undefined
+//alert('abc' * 3); //NaN
+// alert(1 / 0); //infinity
+// alert(-1 / 0); //-infinity
+// alert('2' * '3'); //6
+
+// let ac = '2';
+// let bc = '3';
+// console.log(parseInt(ac) + parseInt(bc));
+
+// let inputNumberA = prompt('Введите число А:');
+// let inputNumberB = prompt('Введите число B:');
+
+
+// console.log(parseInt(inputNumberA) + parseInt(inputNumberB));
+// console.log(parseInt(inputNumberA) - parseInt(inputNumberB));
+// console.log(parseInt(inputNumberA) * parseInt(inputNumberB));
+// console.log(parseInt(inputNumberA) / parseInt(inputNumberB)); 
+// console.log(parseInt(inputNumberA) % parseInt(inputNumberB));
+
+let inputNumber = prompt('Введите число');
+
+if (inputNumber > 5) {
+    console.log('Число больше 5');
+} else if (inputNumber < 5){
+    console.log('Число меньше 5');
+} else {
+    console.log('Число равно 5');
+}
+
